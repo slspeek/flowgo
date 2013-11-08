99f53e872fb4d13fb3ee67bf74e83782ad425918
Steven L. Speek <slspeek@gmail.com>
2013-11-08 19:34:07 +0100

    started

--- uploadhandler.go
+++ uploadhandler.go
@@ -1,12 +1,21 @@
 package flow
 
 import (
+  "github.com/slspeek/goblob"
   "sync"
 )
 
-type Upload struct {
+type upload struct {
   mutex sync.Mutex
   chunks map[int]string
 }
 
+type uploadMap struct {
+  mutex sync.Mutex
+  uploads map[string]upload
+}
 
+type UploadHandler struct {
+  uploads uploadMap
+  bs      goblob.BlobService
+}
